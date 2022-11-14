2b1afa22f0228b51e4fe56cfda83df13e5349572
Karina Undalova <undalova2012@mail.ru>
2022-11-14 21:29:27 +0300

    new

--- SemTask4/Program.cs
+++ SemTask4/Program.cs
@@ -4,33 +4,45 @@ string? inputLineA = Console.ReadLine();
 string? inputLineB = Console.ReadLine();
 string? inputLineC = Console.ReadLine();
 
-if (inputLineA != null&&inputLineB != null&&inputLineC != null ) 
-{
+if (inputLineA != null&&inputLineB != null&&inputLineC != null )
+{ 
     int inputNumberA = int.Parse(inputLineA); 
     int inputNumberB = int.Parse(inputLineB); 
     int inputNumberC = int.Parse(inputLineC); 
+    int inputD = Math.Max(inputNumberA, Math.Max(inputNumberB, inputNumberC));
 
-    if (inputNumberA > inputNumberB)
-    {
-        if (inputNumberA > inputNumberC)
-        {
-            Console.WriteLine("максимальное число " + inputNumberA);
-        }
-        else
-        {
-            Console.WriteLine("максимальное число " + inputNumberC);
-        }
-    }
-    else if(inputNumberB > inputNumberC)
-    {
-
-        Console.WriteLine("максимальное число " + inputNumberB);
-    }
-    else
-    {
-        Console.WriteLine("максимальное число " + inputNumberC);
-    }
+    Console.WriteLine(inputD);
 }
+// (условие)?<значение 1>:<значение 2>
+// int D = ((A>B)?A:B);
+// int R = ((B>C)?B:C);
+// ===============================================================
+// {
+//     int inputNumberA = int.Parse(inputLineA); 
+//     int inputNumberB = int.Parse(inputLineB); 
+//     int inputNumberC = int.Parse(inputLineC); 
+
+//     if (inputNumberA > inputNumberB)
+//     {
+//         if (inputNumberA > inputNumberC)
+//         {
+//             Console.WriteLine("максимальное число " + inputNumberA);
+//         }
+//         else
+//         {
+//             Console.WriteLine("максимальное число " + inputNumberC);
+//         }
+//     }
+//     else if(inputNumberB > inputNumberC)
+//     {
+
+//         Console.WriteLine("максимальное число " + inputNumberB);
+//     }
+//     else
+//     {
+//         Console.WriteLine("максимальное число " + inputNumberC);
+//     }
+// }
 
     
    
